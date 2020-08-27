7c582e16f10b263d21b76c1e7e95f68cdc8ee5c2
lj7788 <lj7788@126.com>
2020-08-27 22:47:41 +0800

    init

---
+++ src/components/MyTest1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="!show" class="my-test1">
+      <h1>this is my test1</h1>
+      <el-button>Hello</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            show:false
+        }
+    }
+}
+</script>
+
+<style lang="less">
+.my-test1{
+    h1{
+        color:red;
+    }
+}
+</style>
